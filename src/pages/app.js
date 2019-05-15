d4640f5506fa0ce73054a121c294ee37e9d805c7
StefiNicoara <StefiNicoara@users.noreply.github.com>
2019-05-15 22:18:14 +0300

    stefi's stuff

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Register from './register/register'
-import Home from './home/home'
+import Home from './home/Home.js'
 function App() {
   return (
       <Home/>
